df36f239eb5c014f7cef97dc32bba2908ffee340
Lucas Coelho <lucasestevaocoelho@gmail.com>
2024-05-10 23:45:59 -0500

    Refact the snaping in place of the pieces to properly update the div

--- src/components/Chessboard/Chessboard.tsx
+++ src/components/Chessboard/Chessboard.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import Tile from '../Tile/Tile'
 import './Chessboard.css'
 
@@ -17,13 +18,11 @@ let activePiece: HTMLElement | null = null;
 const initialPosition = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR";
 // const dificultPosition = "8/5k2/3p4/1p1Pp2p/pP2Pp1P/P4P1K/8/8 b"
 
+const initialBoardState = readBoard(initialPosition)
+
 // Setting up the board
 const verticalAxis = ["1", "2", "3", "4", "5", "6", "7", "8"]
 const horizontalAxis = ["a", "b", "c", "d", "e", "f", "g", "h"]
-const pieces: Piece[] = readBoard(initialPosition)
-
-let oldX = 0
-let oldY = 0
 
 // RowLabel component
 const RowLabel = ({ label }: { label: string }) => {
@@ -43,15 +42,23 @@ const ColumnLabel = ({ label }: { label: string }) => {
 * @returns a div of the entire chessboard with OnMouseMove commands for interaction with pieces
 */
 export default function Chessboard() {
-    let board = [];
-    let rowLabels = [];
-    let columnLabels = [];
+    let board = []
+    let rowLabels = []
+    let columnLabels = []
+
+    const [initialGridX, setGridX] = useState(0)
+    const [initialGridY, setGridY] = useState(0)
+    const [pieces, setPieces] = useState<Piece[]>(initialBoardState)
+
+    useEffect(() => {
+        setPieces(pieces)
+    })
 
     // Generating chessboard tiles
     for (let rank = verticalAxis.length - 1; rank >= 0; rank--) {
         for (let file = 0; file < horizontalAxis.length; file++) {
             let isWhite = (rank + file + 2) % 2 === 0;
-            let image = "";
+            let image = ""
 
             pieces.forEach((piece) => {
                 if (piece.verticalPosition === file && piece.horizontalPosition === rank) {
@@ -87,109 +94,117 @@ export default function Chessboard() {
             </div>
         </div>
     );
-}
 
-/**
+    /**
  * Move pieces function. Called when piece is active (is clicked).
  * Sets the piece div have its location equal to the mouse location.
  * 
  * @param e 
  */
-function movePiece(e: React.MouseEvent) {
-    if (activePiece) {
-        // Calculate the new position relative to the chessboard container
-        const chessboardContainerRect = document.getElementById("chessboard-container")?.getBoundingClientRect();
-        if (!chessboardContainerRect) return;
+    function movePiece(e: React.MouseEvent) {
+        if (activePiece) {
+            // Calculate the new position relative to the chessboard container
+            const chessboardContainerRect = document.getElementById("chessboard-container")?.getBoundingClientRect();
+            if (!chessboardContainerRect) return;
 
-        const offsetX = activePiece.offsetWidth / 2;
-        const offsetY = activePiece.offsetHeight / 2;
-        const x = e.clientX - chessboardContainerRect.left - offsetX;
-        const y = e.clientY - chessboardContainerRect.top - offsetY;
+            const offsetX = activePiece.offsetWidth / 2;
+            const offsetY = activePiece.offsetHeight / 2;
+            const x = e.clientX - chessboardContainerRect.left - offsetX;
+            const y = e.clientY - chessboardContainerRect.top - offsetY;
 
-        // Update the position of the active piece
-        activePiece.style.left = `${x}px`;
-        activePiece.style.top = `${y}px`;
+            // Update the position of the active piece
+            activePiece.style.left = `${x}px`;
+            activePiece.style.top = `${y}px`;
+        }
     }
-}
 
-/**
- * Selects a piece and stores it in a global scope variable.
- * Sets the piece selected to be grabed by the mouse.
- * After it happens, calls the move piece.
- * 
- * @param e 
- */
-function grabPiece(e: React.MouseEvent) {
-    const element = e.target as HTMLElement;
-    if (element.classList.contains("chess-piece")) {
-        // Calculate the position of the chessboard container relative to the viewport
-        const chessboardContainerRect = document.getElementById("chessboard-container")?.getBoundingClientRect();
-        if (!chessboardContainerRect) return;
-
-        // Calculate the size of each grid cell
-        const gridCellWidth = chessboardContainerRect.width / 8;
-        const gridCellHeight = chessboardContainerRect.height / 8;
-
-        // Calculate the position of the clicked grid cell
-        const gridCellX = Math.floor((e.clientX - chessboardContainerRect.left) / gridCellWidth);
-        const gridCellY = Math.floor((e.clientY - chessboardContainerRect.top) / gridCellHeight);
-
-        // Calculate the position of the piece within the grid cell (centered)
-        const offsetX = element.offsetWidth / 2;
-        const offsetY = element.offsetHeight / 2;
-        const x = (gridCellX * gridCellWidth) + (gridCellWidth / 2) - offsetX;
-        const y = (gridCellY * gridCellHeight) + (gridCellHeight / 2) - offsetY;
-
-        oldX = x
-        oldY = y
-
-        // Set the initial position of the piece
-        element.style.position = "absolute";
-        element.style.left = `${x}px`;
-        element.style.top = `${y}px`;
-
-        // Store the active piece
-        activePiece = element;
-    }
-}
-
-/**
- * Function to drop the piece when click is released.
- * 
- * @param e 
- */
-function dropPiece(e: React.MouseEvent) {
-    if (activePiece) {
+    /**
+     * Selects a piece and stores it in a global scope variable.
+     * Sets the piece selected to be grabed by the mouse.
+     * After it happens, calls the move piece.
+     * 
+     * @param e 
+     */
+    function grabPiece(e: React.MouseEvent) {
         const element = e.target as HTMLElement;
-        const chessboardContainerRect = document.getElementById("chessboard-container")?.getBoundingClientRect();
-        if (!chessboardContainerRect) return;
+        if (element.classList.contains("chess-piece")) {
 
-        // Calculate the size of each grid cell
-        const gridCellWidth = chessboardContainerRect.width / 8;
-        const gridCellHeight = chessboardContainerRect.height / 8;
+            // Calculate the position of the chessboard container relative to the viewport
+            const chessboardContainerRect = document.getElementById("chessboard-container")?.getBoundingClientRect();
+            if (!chessboardContainerRect) return;
 
-        // Calculate the position of the clicked grid cell
-        const gridCellX = Math.floor((e.clientX - chessboardContainerRect.left) / gridCellWidth);
-        const gridCellY = Math.floor((e.clientY - chessboardContainerRect.top) / gridCellHeight);
+            // Know what piece you grabed for drop function.
+            setGridX(Math.floor((e.clientX - chessboardContainerRect.left) / (chessboardContainerRect.width / 8)))
+            setGridY(Math.abs(Math.ceil((e.clientY - chessboardContainerRect.top - 800) / (chessboardContainerRect.height / 8))))
 
-        // Calculate the position of the piece within the grid cell (centered)
-        const offsetX = element.offsetWidth / 2;
-        const offsetY = element.offsetHeight / 2;
-        const x = (gridCellX * gridCellWidth) + (gridCellWidth / 2) - offsetX;
-        const y = (gridCellY * gridCellHeight) + (gridCellHeight / 2) - offsetY;
+            const offsetX = element.offsetWidth / 2;
+            const offsetY = element.offsetHeight / 2;
+            const x = e.clientX - chessboardContainerRect.left - offsetX;
+            const y = e.clientY - chessboardContainerRect.top - offsetY;
 
-        if (inBoardBounds(e.clientX, e.clientY)) {
             // Set the initial position of the piece
             element.style.position = "absolute";
             element.style.left = `${x}px`;
             element.style.top = `${y}px`;
+
+            // Store the active piece
+            activePiece = element;
         }
-        else {
-            element.style.position = "absolute";
-            element.style.left = `${oldX}px`;
-            element.style.top = `${oldY}px`;
+    }
+
+    /**
+     * Function to drop the piece when click is released.
+     * 
+     * @param e 
+     */
+    function dropPiece(e: React.MouseEvent) {
+        if (activePiece) {
+            const chessboardContainerRect = document.getElementById("chessboard-container")?.getBoundingClientRect();
+            if (!chessboardContainerRect) return;
+
+            if (inBoardBounds(e.clientX, e.clientY)) {
+                // If dropped inside the board boundaries, update the position of the piece
+                const gridCellX = Math.floor((e.clientX - chessboardContainerRect.left) / (chessboardContainerRect.width / 8));
+                const gridCellY = Math.abs(Math.ceil((e.clientY - chessboardContainerRect.top - 800) / (chessboardContainerRect.height / 8)));
+
+                // Update the position of the piece in the state
+                const updatedPieces = pieces.map(piece => {
+                    if (piece.horizontalPosition === initialGridY && piece.verticalPosition === initialGridX) {
+                        // Snap the piece back to its original position
+                        if ((gridCellX === initialGridX) && (gridCellY === initialGridY)) {
+                            // Calculate the size of each grid cell
+                            const gridCellWidth = chessboardContainerRect.width / 8;
+                            const gridCellHeight = chessboardContainerRect.height / 8;
+
+                            // Calculate the position of the clicked grid cell
+                            const gridCellX = Math.floor((e.clientX - chessboardContainerRect.left) / gridCellWidth);
+                            const gridCellY = Math.floor((e.clientY - chessboardContainerRect.top) / gridCellHeight);
+
+                            // Calculate the position of the piece within the grid cell (centered)
+                            const offsetX = 100 / 2;
+                            const offsetY = 100 / 2;
+
+                            const x = (gridCellX * gridCellWidth) + (gridCellWidth / 2) - offsetX;
+                            const y = (gridCellY * gridCellHeight) + (gridCellHeight / 2) - offsetY;
+
+                            activePiece!.style.left = `${x}px`;
+                            activePiece!.style.top = `${y}px`;
+                        }
+                        else {
+                            piece.verticalPosition = gridCellX
+                            piece.horizontalPosition = gridCellY
+                            setGridX(gridCellX)
+                            setGridY(gridCellY)
+                        }
+                    }
+                    return piece
+                })
+                setPieces(updatedPieces);
+            }
         }
-        activePiece = null
+
+        // Reset the active piece
+        activePiece = null;
     }
 }
 
